65207d0e6b3538c4d43eaaac08db7fef70a43c93
Shrigovinda25 <01fe23bar047@kletech.ac.in>
2026-05-02 01:03:32 +0530

    your message

--- api/index.js
+++ api/index.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.get('/api/health', (req, res) => res.json({ status: 'ok', message: 'BuildVerse API is live' }));
+
 // ----------------------------------------------------------------------------
 // Firebase Initialization
 // ----------------------------------------------------------------------------
